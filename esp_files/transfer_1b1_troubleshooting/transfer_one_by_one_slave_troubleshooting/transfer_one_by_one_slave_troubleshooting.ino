d1c422f7909713435e448ba211656dcfe2c07dbe
Nonmiscelaneous <128332863+Nonmiscelaneous@users.noreply.github.com>
2024-05-10 14:43:21 -0600

    Update transfer_one_by_one_slave_troubleshooting.ino

--- esp_files/transfer_1b1_troubleshooting/transfer_one_by_one_slave_troubleshooting/transfer_one_by_one_slave_troubleshooting.ino
+++ esp_files/transfer_1b1_troubleshooting/transfer_one_by_one_slave_troubleshooting/transfer_one_by_one_slave_troubleshooting.ino
@@ -1,12 +1,17 @@
 #include <ESP32SPISlave.h>
 #include "helper.h"
 
+// MOSI 13
+// MISO 12
+// SCLK 14
+// CS 15
+
 ESP32SPISlave slave;
 
-static constexpr size_t BUFFER_SIZE = 2;
+static constexpr size_t BUFFER_SIZE = 4;
 static constexpr size_t QUEUE_SIZE = 1;
-uint8_t tx_buf[BUFFER_SIZE] {0};
-uint8_t rx_buf[BUFFER_SIZE] {0};
+uint8_t tx_buf[BUFFER_SIZE] {170, 170,170,170};
+uint8_t rx_buf[BUFFER_SIZE] {0, 0, 0 ,0};
 
 void setup()
 {
@@ -38,6 +43,8 @@ void loop()
     // verify and dump difference with received data
     if (verifyAndDumpDifference("slave", tx_buf, BUFFER_SIZE, "master", rx_buf, received_bytes)) {
         Serial.println("successfully received expected data from master");
+        dumpBuffers("tx", tx_buf, 0,sizeof(tx_buf));
+        dumpBuffers("rx", rx_buf, 0,sizeof(rx_buf));
     } else {
         Serial.println("unexpected difference found between master/slave data");
         dumpBuffers("tx", tx_buf, 0,sizeof(tx_buf));
